f4d9873d2fb24d096af98822954c605decbdb6a8
Yusuf Celep <yusufcelep8@gmail.com>
2020-02-19 23:47:15 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,7 +1,7 @@
 class EmailAddressParser
   attr_accessor :email_addresses
   
-  def email_addresses=(email_addresses)
+  def initialize=(email_addresses)
     @email_addresses = email_addresses
   end
   
