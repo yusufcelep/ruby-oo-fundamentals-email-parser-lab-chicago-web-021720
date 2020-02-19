5a85ed262596bc96daf77abdc2f32a86f592a16d
Yusuf Celep <yusufcelep8@gmail.com>
2020-02-19 23:50:28 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -6,24 +6,12 @@ class EmailAddressParser
   end
   
   def parser
-    email_addresses.delete(',').split.uniq
-  end
-end
-
-class EmailParser
-
-  attr_accessor :email
-  def  initialize(emails)
-    @email = emails
-  end
-
-  def parse
     email_array = @email.split(/[, ]/).uniq
     email_array.reject! {|element| element.empty?}
     email_array
   end
-
 end
+
 
 # Build a class EmailParser that accepts a string of unformatted 
 # emails. The parse method on the class should separate them into
