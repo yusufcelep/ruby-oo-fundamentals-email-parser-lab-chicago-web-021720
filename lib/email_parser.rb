0260d854ea832e8207d17575468c5dcb15a8b258
Yusuf Celep <yusufcelep8@gmail.com>
2020-02-20 00:00:27 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -12,7 +12,6 @@ class EmailAddressParser
   end
 end
 
-
 # Build a class EmailParser that accepts a string of unformatted 
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
